9b4f28cfdaa83d2efa6fe964000b5dc16fb0f64a
GaragesWirral <newsletterproductoffers@gmail.com>
2025-06-07 13:23:57 +0100

    Update worker.js to properly serve static files

--- worker.js
+++ worker.js
@@ -1,7 +1,15 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+    let path = url.pathname;
+    
+    // Serve index.html for root path
+    if (path === '/') {
+      path = '/index.html';
+    }
+
     try {
-      return await env.ASSETS.fetch(request);
+      return await env.ASSETS.fetch(new Request(new URL(path, request.url)));
     } catch (e) {
       return new Response(`${request.url} not found`, {
         status: 404,
